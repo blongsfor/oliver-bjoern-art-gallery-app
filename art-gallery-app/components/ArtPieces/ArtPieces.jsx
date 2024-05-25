a52c1fed4a2cd2195e8bf408f4811e72ebc234c6
Oliver Schupke <solanx@web.de>
2024-05-25 20:57:15 +0200

    Fix: Provide correct value for the key attribute in order to have a unique identifier for all list items

--- art-gallery-app/components/ArtPieces/ArtPieces.jsx
+++ art-gallery-app/components/ArtPieces/ArtPieces.jsx
@@ -1,13 +1,11 @@
 import ArtPiecePreview from "../ArtPiecePreview/ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
-  //   console.log("Pieces: ", pieces());
 
-  console.log("fetched", typeof fetchedPieces);
   return (
     <ul>
       {pieces.map((piece) => (
-        <li key={pieces.slug}>
+        <li key={piece.slug}>
           <ArtPiecePreview
             image={piece.imageSource}
             title={piece.name}
