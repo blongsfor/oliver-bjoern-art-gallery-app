c7d37c9131b1a303a691b1c10a518891bd5e0ba8
Oliver Schupke <solanx@web.de>
2024-05-26 03:16:26 +0200

    Refactor: Add clickable link element to image for directing to details page of an art piece

--- art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.jsx
+++ art-gallery-app/components/ArtPiecePreview/ArtPiecePreview.jsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import Image from "next/image";
 
-export default function ArtPiecePreview({ image, title, artist }) {
+export default function ArtPiecePreview({ image, title, artist, slug }) {
   return (
     <>
-      <Image src={image} width={500} height={500} alt={title}></Image>
+      <Link href={`art-pieces/${slug}`}>
+        <Image src={image} width={500} height={500} alt={title}></Image>
+      </Link>
       <h3>{title}</h3>
       <p>{artist}</p>
     </>
