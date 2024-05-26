4db376a4659c0a25d494af4d3792cb82efea15ad
Bjoern <bjoern.n@mailbox.org>
2024-05-26 14:36:30 +0200

    rename Homepage to SpotlightPage and render only spotlight component on startup

--- art-gallery-app/pages/index.js
+++ art-gallery-app/pages/index.js
@@ -3,13 +3,11 @@ import { useEffect, useState } from "react";
 import ArtPieces from "@/components/ArtPieces/ArtPieces";
 import Spotlight from "@/components/Spotlight/Spotlight";
 
-export default function HomePage({ data }) {
+export default function SpotlightPage({ data }) {
   return (
     <div>
       <h1>Gallery App</h1>
       <Spotlight pieces={data} />
-      <h2>Art Pieces</h2>
-      <ArtPieces pieces={data} />
     </div>
   );
 }
