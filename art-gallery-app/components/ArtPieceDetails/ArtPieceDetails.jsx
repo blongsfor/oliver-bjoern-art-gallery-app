edc0e6c76818fdc3869fd9fefa02a241daf340db
Oliver Schupke <solanx@web.de>
2024-05-27 20:30:23 +0200

    Refactor: Change path for the backlink from '/' to 'art-pieces' to get directed to the art pieces page instead of the spotlight page according to user story 4 requirements

--- art-gallery-app/components/ArtPieceDetails/ArtPieceDetails.jsx
+++ art-gallery-app/components/ArtPieceDetails/ArtPieceDetails.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
     <>
-      <Link href={"/"}>Back</Link>
+      <Link href={"/art-pieces"}>Back</Link>
       <Image src={image} width={500} height={500} alt={title}></Image>
       <h3>Title: {title}</h3>
       <p>Artist: {artist}</p>
