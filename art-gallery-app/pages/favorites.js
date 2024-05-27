68c0e4d0ddc78d2f69c7944ee16f849f056a0af7
Oliver Schupke <solanx@web.de>
2024-05-27 22:56:09 +0200

    Feat: Implement the favorites functionality for the entire website, including the favorites icon that reflects the respective state

--- art-gallery-app/pages/favorites.js
+++ art-gallery-app/pages/favorites.js
@@ -1,65 +1,14 @@
 import ArtPieces from "@/components/ArtPieces/ArtPieces";
-import { useState } from "react";
 
-export default function Favorites({
-  artPiecesInfos,
-  onToggleFavorite,
-  pieces,
-}) {
-  const [artPieces, setArtPieces] = useState([]);
-
-  const favoritePieces = pieces.filter((piece) => {
-    const foundArtPiece = artPiecesInfos.find(
-      (artPiece) => artPiece.slug === piece.slug
-    );
-    return foundArtPiece && foundArtPiece.isFavorite;
-  });
-
-  console.log("artPiecesInfos favorites.js: ", artPiecesInfos);
-
-  // function handleToggleFavorite(slug) {
-  //   // See if the art piece is already in the array
-  //   const info = artPieces.find((art) => art.slug === slug);
-  //   if (info) {
-  //     // If the art piece is already in the array, toggle the isFavorite value
-  //     const newInfo = artPieces.map((artPiece) =>
-  //       artPiece.slug === slug
-  //         ? { ...artPiece, isFavorite: !artPiece.isFavorite }
-  //         : artPiece
-  //     );
-  //     setArtPieces(newInfo);
-  //     console.log("afterClickIf", artPieces);
-  //   } else {
-  //     // If the art piece is not in the array already, add it with the favorite as true
-  //     const newInfo = [...artPieces, { slug, isFavorite: true }];
-  //     setArtPieces(newInfo);
-  //     console.log("afterClickElse", artPieces);
-  //   }
-  // }
-
-  // function handleToggleFavorite(slug) {
-  //   const foundArtPiece = artPieces.find(
-  //     (artPiece) => artPiece.slug === slug
-  //   );
-  //   if (foundArtPiece) {
-  //     setArtPieces(
-  //       artPieces.map(
-  //         (artPiecesInfo) =>
-  //           artPiecesInfo.slug === slug && {
-  //             ...artPiecesInfo,
-  //             isFavorite: !artPiecesInfo.isFavorite,
-  //           }
-  //       )
-  //     );
-  //   } else {
-  //     setArtPieces([...artPieces, { slug, isFavorite: true }]);
-  //   }
-  // }
-
+export default function Favorites({ artPiecesInfos, onToggleFavorite }) {
+  const favoritePieces = artPiecesInfos.filter((piece) => piece.isFavorite);
 
   return (
-    <>
-      <ArtPieces pieces={favoritePieces} />
-    </>
+    <ArtPieces
+      pieces={favoritePieces}
+      onToggleFavorite={onToggleFavorite}
+    />
   );
 }
+
+
