bbf8ae01469babe1275649302b8e8b1c2b5c570e
Oliver Schupke <solanx@web.de>
2024-05-28 20:21:59 +0200

    Feat: Add toggle favorite functionality and colors section in ArtPieceDetails page

--- art-gallery-app/components/ArtPieceDetails/ArtPieceDetails.jsx
+++ art-gallery-app/components/ArtPieceDetails/ArtPieceDetails.jsx
@@ -1,8 +1,27 @@
+import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import CommentForm from "../CommentForm/CommentForm";
 import Comments from "../Comments/Comments";
+
+const ColorsContainer = styled.div`
+  text-align: center;
+  margin-block-start: -20px;
+  margin-block-end: 50px;
+`;
+
+const ColorChips = styled.div`
+  --size: 2.5rem;
+
+  display: inline-block;
+  margin-inline: calc(var(--size) / 5);
+  width: var(--size);
+  height: var(--size);
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  outline: 2px black solid;
+`;
 
 export default function ArtPieceDetails({
   image,
@@ -11,11 +30,11 @@ export default function ArtPieceDetails({
   year,
   genre,
   slug,
+  colors,
   isFavorite,
   onToggleFavorite,
   comments,
   onSubmitComment,
-  artPiecesInfos,
 }) {
   return (
     <>
@@ -30,16 +49,25 @@ export default function ArtPieceDetails({
           height={500}
           alt={title}
         ></Image>
-        <div className="details" style={{ marginTop: "20px" }}>
+        <FavoriteButton
+          isFavorite={isFavorite}
+          onToggleFavorite={onToggleFavorite}
+          slug={slug}
+        />
+        <div className="details">
           <h3>Title: {title}</h3>
           <p>Artist: {artist}</p>
           <p>Year: {year}</p>
           <p>Genre: {genre}</p>
         </div>
       </div>
+      <ColorsContainer>
+        {colors.map((color, idx) => (
+          <ColorChips key={idx} color={color} />
+        ))}
+      </ColorsContainer>
       <div className="comment-container">
         <CommentForm onSubmitComment={onSubmitComment} slug={slug} />
-
         <Comments comments={comments} />
       </div>
     </>
